58fe330bbf91050ebe260e8620f893d1107b5705
franesac <estradakiko@gmail.com>
2015-09-13 11:07:44 -0500

    time 1

---
+++ plot1.R
@@ -0,0 +1,23 @@
+# Generating Plot 1
+
+## Getting full dataset
+data_full <- read.csv("household_power_consumption.txt", header = T, sep = ';', 
+                      na.strings = "?", nrows = 2075259, check.names = F, 
+                      stringsAsFactors = F, comment.char = "", quote = '\"')
+data_full$Date <- as.Date(data_full$Date, format = "%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(data_full, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+
+
+
+## Plot 1
+png(filename="plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+dev.off()
